66e17d0a15a2d7bbc6e0a853417bd37ce5eea604
YooMyeongSeon <s.cnelly@hotmail.com>
2023-08-09 22:45:38 +0900

    Test commit 5

--- src/main/java/com/project/yoo/main/MainController.java
+++ src/main/java/com/project/yoo/main/MainController.java
@@ -1,13 +1,17 @@
 package com.project.yoo.main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping("/")
-	public String Main() {
+	public String Main(Model model) {
+		String msg = "Hello?";
+		
+		model.addAttribute("msg", msg);
 		return "main/mainPage";
 	}
 	
