bc9fcf13ac028a7bd1637008cd60abcdbd2ff520
YooMyeongSeon <s.cnelly@hotmail.com>
2023-08-09 22:06:59 +0900

    first commit

---
+++ src/test/java/com/project/yoo/YooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
